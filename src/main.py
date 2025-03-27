02ca38265d18cb11015b358a701ff047be3ded5a
Semyon Zyubin <lesqt58@gmail.com>
2025-03-27 11:30:56 +0000

    feat: module model.py

--- src/main.py
+++ src/main.py
@@ -5,14 +5,13 @@ Description: This module contains the main function for the project.
 
 from  argparse import ArgumentParser
 
+from src.model import model
+
 parser = ArgumentParser(description="Linear Model")
 parser.add_argument("--weight", type=float, default=2.0, help="Weight of the model")
 parser.add_argument("--intercept", type=float, default=0.0, help="Intercept of the model")
 
 args = parser.parse_args()
-
-def model(x:float, weight:float = 2.0, intercept:float = 0.0):
-    return weight * x + intercept
 
 input_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 output_data = [model(x, args.weight, args.intercept) for x in input_data]
---
+++ src/model.py
@@ -0,0 +1,7 @@
+"""
+Module: model.py
+Description: This module contains the model function for the project"
+"""
+
+def model(x:float, weight:float = 2.0, intercept:float = 0.0):
+    return weight * x + intercept

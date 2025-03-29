f9b72842843adfde2170c2d7143a66dd14d52949
Semyon Zyubin <lesqt58@gmail.com>
2025-03-29 10:41:39 +0000

    feat: add loging to main.py

--- src/main.py
+++ src/main.py
@@ -3,11 +3,18 @@ Module: main.py
 Description: This module contains the main function for the project.
 """
 
+import os
 from  argparse import ArgumentParser, Namespace
 import sys
 
+from loguru import logger
+
 from src.model import Model
 from src.data import read_data, write_data
+
+LOGS_PATH = os.path.join("logs", "main.log")
+logger.add(LOGS_PATH, level="DEBUG", rotation="10 MB")
+
 
 def parse_args() -> Namespace:
     """
@@ -26,27 +33,40 @@ def main() -> None:
     """
     Main function
     """
+    logger.info("Starting the main function")
     args = parse_args()
+
     weight = args.weight
     intercept = args.intercept
     input_path = args.input
     output_path = args.output
 
-    model = Model(weight, intercept)
-    try:
-        input_data = read_data(input_path)
-        output_data = model.predict(input_data)
-        write_data(output_path, output_data)
+    logger.debug(f'Weight: {weight}')
+    logger.debug(f'Intercept: {intercept}')
+    logger.debug(f'Input path: {input_path}')
+    logger.debug(f'Output path: {output_path}')
 
-        print(output_data)
-        print('Successfully wrote output data to', output_path)
+    logger.info("Creating the model")
+    model = Model(weight, intercept)
+    logger.info("Model created successfully.")
+
+    try:
+        logger.info(f"Reading input data from {input_path}")
+        input_data = read_data(input_path)
+
+        logger.info("Predicting output data.")
+        output_data = model.predict(input_data)
+        logger.success("Precision completed successfully")
+
+        write_data(output_path, output_data)
+        logger.success(f'Successfully wrote output data to {output_path}')
 
     except FileNotFoundError:
-        print("Input file not found.")
+        logger.error(f"Input file {input_path} not found.")
         sys.exit(1)
 
     except Exception as e:
-        print('An error occurred', str(e))
+        logger.error(f'An error occurred: {str(e)}')
         sys.exit(1)
 
 if __name__ == "__main__":
